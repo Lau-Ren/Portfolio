f0db8b594787fbb826c0dc6710fdb3b45b440fc1
Lauren Colls <lauren.colls@hotmail.com>
2016-01-18 18:38:36 +1300

    add empty files and folders

--- js/Controllers/MainController.js
+++ js/Controllers/MainController.js
@@ -1,5 +1,19 @@
 app.controller('MainController', ['$scope', function($scope) { 
   $scope.title = 'Web Projects'; 
+
+  $scope.projects = [ 
+  { 
+    name: 'Anytime Computers Services Limited', 
+    screenShot: 'images/img.jpg',
+    
+  }, 
+  { 
+    name: 'Bryson Rooney', 
+    screenShot: 'imgages/img.jpg',
+    
+  }
+   
+];
 }]);
 
 
